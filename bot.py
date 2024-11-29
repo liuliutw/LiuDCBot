1cf138cdf108a5785e64e0e7d86c71006b3aaa96
嚴取裂痕 <bliss106046@gmail.com>
2024-11-29 23:47:27 +0800

    fix : discord-nevfile
    修正 : Discord Token 外洩霧提
    修正 : 程式排序邏輯

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
+import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!',intents=intents)
+intents.members = True  # 啟用成員事件
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(">>bot is online<<")
+    print(">> Bot is online <<")
 
 @bot.event
 async def on_member_join(member):
-    print(f'{member}join!')
-    #channel = bot.get_channel(1312070988665716817)
-    #await channel.send(f'{member}join!')
+    print(f'{member} join!')
+    channel = bot.get_channel(1307355346469130293)  # 替換為正確的頻道 ID
+    if channel:
+        await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    print(f'{member}leave!')
-    #channel = bot.get_channel(1312071051475292201)
-    #await channel.send(f'{member}leave!')
+    print(f'{member} leave!')
+    channel = bot.get_channel(1307355346469130293)  # 替換為正確的頻道 ID
+    if channel:
+        await channel.send(f'{member} leave!')
 
-bot.run('MTMwNzM1NDQ5MjM5NTg0Nzc3Mg.GRSML8.yE4pyIDSvBx4NAQVkXW4IDeTH9x_MSywQOlajs')
+bot.run(TOKEN)
