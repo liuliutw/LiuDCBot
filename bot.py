1cb02dd5fecd824576797791f18f654631162ac1
liuliutw <liul85661@gmail.com>
2024-11-30 21:14:50 +0800

    可發送圖片 TOKEN修正

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.all()
 intents.members = True  # 啟用成員事件
@@ -13,14 +18,14 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} join!')
-    channel = bot.get_channel(1312070988665716817)  # 替換為正確的頻道 ID
+    channel = bot.get_channel(int(jdata['welcome']))  # 替換為正確的頻道 ID
     if channel:
         await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
     print(f'{member} leave!')
-    channel = bot.get_channel(1312071051475292201)  # 替換為正確的頻道 ID
+    channel = bot.get_channel(int(jdata['leave']))  # 替換為正確的頻道 ID
     if channel:
         await channel.send(f'{member} leave!')
 
@@ -28,3 +33,10 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} (ms)')
 
+@bot.command()
+async def 你是哪一派(ctx):
+    radomImage=random.choice(jdata['image1'])
+    image=discord.File(radomImage)
+    await ctx.send(file=image)
+
+bot.run(jdata['TOKEN'])
